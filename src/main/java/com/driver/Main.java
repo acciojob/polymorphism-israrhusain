e5fcf67921bf147c2ea36605a1a5f919b9e78f69
israr husain <54683061+israrhusain@users.noreply.github.com>
2022-10-22 21:24:40 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,15 @@ package com.driver;
 public class Main {
   
   public class Product{
-    
+    public int product(int x, int y) {}
+    public int product(int x, int y, int z) {}
+     public double product(double x, double y) {} 
   }
+  public static void main(String[] args){
+      Product p=new Product();
+       p.product(2,3);
+       p.product(2,4,5);
+       p.product(2.45,3.56);
+   }
 
 }
