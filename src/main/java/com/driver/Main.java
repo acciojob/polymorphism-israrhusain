e37cb05ace3ee0e31761f1eea3cf0fd8fc9c4ad6
israr <israrhusain5892@gmail.com>
2022-12-14 19:15:53 +0530

    messade

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,13 +4,13 @@ public class Main {
   
   public static class Product{
     public int product(int x, int y) {
-        return 0;
+        return x+y;
     }
     public int product(int x, int y, int z) {
-        return 0;
+        return x+y+z;
     }
      public double product(double x, double y) {
-         return 0.0;
+         return x+y;
      }
   }
   public static void main(String[] args){
