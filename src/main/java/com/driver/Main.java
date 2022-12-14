7e0a32868de091fbb54f81fe0dd009e90f29a132
israr <israrhusain5892@gmail.com>
2022-12-14 18:59:21 +0530

    success

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
   
-  public class Product{
+  public static class Product{
     public int product(int x, int y) {}
     public int product(int x, int y, int z) {}
      public double product(double x, double y) {}
